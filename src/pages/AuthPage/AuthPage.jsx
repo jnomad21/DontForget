0e6d0f59c05aa4ec4b9969427fdd0062797efa0a
Jason McGrath <jnomad21@yahoo.com>
2023-06-11 15:26:01 -0700

    removed background styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,7 +6,7 @@ export default function AuthPage({setUser}) {
     
   return (
     <>
-    <body style = {{backgroundColor: `rgb(60, 130, 192, .2)`}}>
+    <body>
       <h1 id='AuthPageH1'>Register/LogIn</h1>
       <SignUpForm setUser ={setUser}/>
       <LoginForm setUser ={setUser}/>
