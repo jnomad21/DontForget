9245ae38542f592901acc6bd10097ece94abd844
Jason McGrath <jnomad21@yahoo.com>
2023-06-30 10:17:50 -0700

    navbar formatting

--- src/components/NavBar/UnloggedNavBar.jsx
+++ src/components/NavBar/UnloggedNavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 import './NavBar.css'
+import './UnloggedNavBar.css'
 <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Racing+Sans+One&display=swap" rel="stylesheet"></link>
 
 export default function UnloggedNavBar(){
@@ -10,12 +11,12 @@ export default function UnloggedNavBar(){
         <div>
           <Link className='Logo' to="/"><h4 id='logo'>DontForget</h4></Link>
         </div>
-        <ul className='navMid'>
+        <ul className='navMid unloggedNav'>
           <li><Link className="link" to="/events">Upcoming Events</Link></li>
           <li><Link className="link" to="/events/calendar">My Calendar</Link></li>
           <li><Link className="link" to="/events/new">New Event</Link></li>
           </ul>
-        <ul className="navRight">
+        <ul id='unloggedNavRight' className="navRight">
           </ul>
         
       </nav>
